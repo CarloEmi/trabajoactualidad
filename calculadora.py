5004c9dfb76330a3b224024d6e3fbde3745b8d37
carlos <carlosspereyra.cp@gmail.com>
2019-05-08 14:58:25 -0300

    ajustes

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+num1 = int (input("ingrese un valor:"))
+num2 = int (input("ingrese un segundo valor:"))
+print ("el primer valor ingresado es:"+ str(num1));
+print ("el segundo valor ingresado es:"+str(num2));
+suma=num1+num2
+print("la suma es igual a :"+str(suma));
